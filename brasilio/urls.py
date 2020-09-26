5ea43e9cee284de636ec819aecdc3fc1741a1a4f
diegofsousa <diegofernando5672@gmail.com>
2020-09-26 18:56:40 -0300

    Adicionando favicon ao sistema de adminstraçao

--- brasilio/urls.py
+++ brasilio/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,6 +12,7 @@ urlpatterns = [
     path("django-rq/", include("django_rq.urls")),
     path("markdownx/", include("markdownx.urls")),
     path("", include("core.urls", namespace="core")),
+    path("favicon.ico", RedirectView.as_view(url=settings.STATIC_URL + "img/favicon.ico")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler403 = "traffic_control.handlers.handler_403"
